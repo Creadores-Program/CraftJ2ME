81fbf32f2856d80b12194fd898452bac31851f43
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2025-07-09 22:44:33 -0600

    Test2 de JavaFX

--- CraftJ2MEProxy/src/main/java/org/CreadoresProgram/CraftJ2ME/renderVista/RenderMCJ2ME.java
+++ CraftJ2MEProxy/src/main/java/org/CreadoresProgram/CraftJ2ME/renderVista/RenderMCJ2ME.java
@@ -8,6 +8,8 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Base64;
 
+import javafx.stage.Stage;
+
 import org.CreadoresProgram.CraftJ2ME.player.Player;
 import org.CreadoresProgram.CraftJ2ME.Proxy;
 
